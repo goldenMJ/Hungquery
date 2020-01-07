4226018dfca0ba5ca233472e920526e7cefb0790
goldenMJ <kadar.s.amek@gmail.com>
2020-01-07 14:00:50 +0100

    adding web:node to Procfile

--- static/js/bootstrap.bundle.js
+++ static/js/bootstrap.bundle.js
@@ -3,6 +3,10 @@
   * Copyright 2011-2019 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
   */
+
+"scripts": {"start": "node server.js"
+}
+
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('jquery')) :
   typeof define === 'function' && define.amd ? define(['exports', 'jquery'], factory) :
